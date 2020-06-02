1c65c6b30e076fa9882110851ca84d97ef3876be
MhmdN <52784514+MhmdN@users.noreply.github.com>
2020-06-03 03:40:45 +0430

    Add files via upload

---
+++ client5.py
@@ -0,0 +1,27 @@
+import socket
+
+host = '127.0.0.1'
+port = 12345
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((host, port))
+while True:
+    num = str(s.recv(1024).decode('ascii'))
+    if num == 'Server is Busy!\n':
+        print(num)
+        break
+    else:
+        print(num)
+    message = input("Enter First Num:\n")
+    s.sendall(message.encode('ascii'))
+    message = input("Enter Second Num:\n")
+    s.sendall(message.encode('ascii'))
+    num = str(s.recv(1024).decode('ascii'))
+    print('Answer is: ', num, '\n')
+    num = input('Do you want to continue(y/n): ')
+    if num == 'y':
+        continue
+    else:
+        s.sendall(b'DONE')
+        break
+
+s.close()
